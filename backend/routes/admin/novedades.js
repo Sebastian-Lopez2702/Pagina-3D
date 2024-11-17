ef5984e33a3817f8038c60377dc5532c7f414982
Sebastian-Lopez2702 <sebaale2002@gmail.com>
2024-11-17 19:51:40 -0300

    conexion con la db y listado de las novedades

--- backend/routes/admin/novedades.js
+++ backend/routes/admin/novedades.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel.js');
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
-        user: req.session.user
+        user: req.session.user,
+        novedades
     });
 });
+
+router.get('/agregar', (req, res, next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    })
+})
 
 module.exports = router;

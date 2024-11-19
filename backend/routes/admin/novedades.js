d623dcebc41553931ad88664c52fc33a844eb2a5
Sebastian-Lopez2702 <sebaale2002@gmail.com>
2024-11-18 22:53:08 -0300

    conexion con cloudinary y en la tabla se muestra la imagen cargada

--- backend/routes/admin/novedades.js
+++ backend/routes/admin/novedades.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel.js');
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
 
 router.get('/', async function (req, res, next) {
 
     var novedades = await novedadesModel.getNovedades();
+
+    novedades = novedades.map(novedad => {
+        if (novedad.img_id) {
+            const imagen = cloudinary.image(novedad.img_id, {
+                width: 100,
+                height: 100,
+                crop: 'pad'
+            });
+            return {
+                ...novedad,
+                imagen
+            }
+        } else {
+            return {
+                ...novedad,
+                imagen: ''
+            }
+        }
+    });
 
     res.render('admin/novedades', {
         layout: 'admin/layout',
@@ -21,15 +43,25 @@ router.get('/agregar', (req, res, next) => {
 
 router.post('/agregar', async (req, res, next) => {
     try {
+
+        var img_id = '';
+        if (req.files && Object.keys(req.files).length > 0) {
+            imagen = req.files.imagen;
+            img_id = (await uploader(imagen.tempFilePath)).public_id;
+        }
+
         if (req.body.titulo != "" && req.body.cuerpo != "") {
-            await novedadesModel.insertNovedad(req.body);
+            await novedadesModel.insertNovedad({
+                ...req.body,
+                img_id
+            });
 
             res.redirect('/admin/novedades');
         } else {
             res.render('admin/agregar', {
                 layout: 'admin/layout',
                 error: true,
-                message: 'Todos los campos son requeridos'
+                message: 'Los campos Titulo y Cuerpo son obligatorios'
             })
         }
     } catch (error) {

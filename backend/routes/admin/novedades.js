d21f17a314b3f0d75b4760003447694fe73e4215
Sebastian-Lopez2702 <sebaale2002@gmail.com>
2024-11-18 23:36:46 -0300

    se agrego la funcionalidad de poder modificar novedades agregando una imagen y ademas, eliminar las novedades con las imagenes

--- backend/routes/admin/novedades.js
+++ backend/routes/admin/novedades.js
@@ -4,6 +4,7 @@ var novedadesModel = require('../../models/novedadesModel.js');
 var util = require('util');
 var cloudinary = require('cloudinary').v2;
 const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 router.get('/', async function (req, res, next) {
 
@@ -77,6 +78,12 @@ router.post('/agregar', async (req, res, next) => {
 router.get('/eliminar/:id', async (req, res, next) => {
     var id = req.params.id;
 
+    let novedad = await novedadesModel.getNovedadById(id);
+
+    if (novedad.img_id) {
+        await (destroy(novedad.img_id));
+    }
+
     await novedadesModel.deleteNovedadById(id);
     res.redirect('/admin/novedades');
 })
@@ -95,11 +102,30 @@ router.get('/modificar/:id', async (req, res, next) => {
 router.post('/modificar', async (req, res, next) => {
     try {
 
+        let img_id = req.body.img_original;
+        let borrar_img_vieja = false;
+
+        if (req.body.img_delete === "1") {
+            img_id = null;
+            borrar_img_vieja = true;
+        } else {
+            if (req.files && Object.keys(req.files).length > 0) {
+                imagen = req.files.imagen;
+                img_id = (await uploader(imagen.tempFilePath)).public_id;
+                borrar_img_vieja = true;
+            }
+        }
+
+        if (borrar_img_vieja && req.body.img_original) {
+            await (destroy(req.body.img_original));
+        }
+
         var id = req.body.id;
 
         var obj = {
             titulo: req.body.titulo,
             cuerpo: req.body.cuerpo,
+            img_id
         }
 
         await novedadesModel.changeNovedadById(obj, id);

fc92173f6b7dbd20e464733426e3ebaaadd13661
Sebastian-Lopez2702 <sebaale2002@gmail.com>
2024-11-19 18:53:51 -0300

    se implemento la api para que conecte con el front

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fileUpload = require('express-fileupload');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/admin/login');
 var adminRouter = require('./routes/admin/novedades');
+var apiRouter = require('./routes/api');
 
 var app = express();
 var session = require('express-session');
@@ -54,6 +56,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/login', loginRouter);
 app.use('/admin/novedades', secured, adminRouter);
+app.use('/api', cors(), apiRouter);
 
 app.use(function (req, res, next) {
   next(createError(404));
